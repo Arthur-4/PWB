32f293251c84274d88326138168c0cf99fc366ed
Arthur-4 <arthur290402@gmail.com>
2023-03-22 19:26:10 -0300

    teste

--- formulario/urls.py
+++ formulario/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('formulario/', views.form_formulario, name = 'formulario'),
+    path('formulario/', views.form_formulario, name = 'formulario'), 
 ]
